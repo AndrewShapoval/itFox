ac410116c56be4c9eca78f5879a2ff9e8e458afa
AndrewShapoval <andrewshapoval88@gmail.com>
2020-12-26 21:36:17 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+import {Text, View, StyleSheet} from "react-native";
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+        <View style={styles.container}>
+          <Text>Hello!</Text>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+// <SafeAreaProvider>
+//   <Navigation colorScheme={colorScheme} />
+//   <StatusBar />
+// </SafeAreaProvider>
